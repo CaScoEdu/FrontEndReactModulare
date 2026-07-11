5a6917a2a567648bff81a465fa4afba6da11994e
carlo_edu <carlo.scocco@einsteinvimercate.edu.it>
2026-07-11 12:08:12 +0200

    corretto errore nome icone

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,8 @@ export default defineConfig({
         description: 'gestione del menu delle pizze',
         theme_color: '#ffffff',
         icons: [
-          { src: 'android-chrome-192x192', sizes: '192x192', type: 'image/png' },
-          { src: 'android-chrome-512x512', sizes: '512x512', type: 'image/png' }
+          { src: 'android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'android-chrome-512x512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
